14c384cbc97cb9ff771a79805ecd23885c4c0154
Giiii <giiselefurtado@hotmail.com>
2014-07-15 12:22:07 -0300

    versao de teste da fila ordenada

--- src/utils/Fila.java
+++ src/utils/Fila.java
@@ -1,46 +1,76 @@
 package utils;
 
 public class Fila implements IFila {
+
     private int tamanho, fim = 0, inicio = 0;
     private boolean cheio = false;
-    private String [] alunos;
+    private String[] alunos;
 
-    public Fila (int tamanho) {
+    public Fila(int tamanho) {
         this.tamanho = tamanho;
         this.alunos = new String[tamanho];
     }
 
-    public int getTamanho () {
+    public int getTamanho() {
         return this.tamanho;
     }
 
-    public void enfileirar (String nome) {
-        if ( ! this.cheio) {
+    public void enfileirar(String nome) {
+        if (!this.cheio) {
             this.alunos[this.fim] = nome;
             this.fim = (this.fim + 1) % this.alunos.length;
-            if (this.fim == this.inicio)
+            if (this.fim == this.inicio) {
                 this.cheio = true;
+            }
         }
     }
 
-    public String desenfileirar () {
+    public String desenfileirar() {
         this.cheio = false;
         return this.alunos[this.inicio++];
     }
 
-    public String toString () {
+    public String toString() {
         String resultado = "";
-        if ( ! this.cheio && this.fim == this.inicio)
+        if (!this.cheio && this.fim == this.inicio) {
             return resultado;
+        }
         if (this.fim > this.inicio) {
-            for (int i = this.inicio; i < this.fim; i++)
-                    resultado += this.alunos[i] + ", ";
+            for (int i = this.inicio; i < this.fim; i++) {
+                resultado += this.alunos[i] + ", ";
+            }
         } else {
             int tamanho = this.alunos.length - this.inicio + this.fim;
-            for (int i = 0; i < tamanho; i++)
+            for (int i = 0; i < tamanho; i++) {
                 resultado += this.alunos[
                     (this.inicio + i) % this.alunos.length] + ", ";
+            }
         }
         return resultado.substring(0, resultado.length() - 2);
+    }
+
+    protected String[] getAlunos() {
+        return alunos;
+    }
+
+    protected void setAlunos(String[] alunos) {
+        this.alunos = alunos;
+    }
+
+    protected void setFim(int fim) {
+        this.fim = fim;
+    }
+
+    protected void setCheio(boolean cheio) {
+        this.cheio = cheio;
+    }
+
+    protected int getInicio() {
+        return this.inicio;
+    }
+
+    protected boolean isCheio() {
+        return this.cheio;
+
     }
 }
