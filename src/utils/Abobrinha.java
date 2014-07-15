76e374fcf76c95b3f6ab10138fd72a7867499f58
Lucas Firak <lucasfirak@hotmai.com>
2014-07-15 10:34:58 -0700

    Firak & Rafao
    teste composite

--- src/utils/Abobrinha.java
+++ src/utils/Abobrinha.java
@@ -2,7 +2,7 @@ package utils;
 
 class Abobrinha {
     public static void main (String[] args) {
-        IFila n = new FilaProxy(new Fila(5));
+        IFila n = new FilaProxy2(new Fila(5));
 
         n.enfileirar("Gabriel");
         n.enfileirar("Gisele");
